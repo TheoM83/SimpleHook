ca4ca921a8b6286839ee74714cb569c896087680
Théo <theo.moulaert1@gmail.com>
2022-07-29 00:47:10 +0200

    V1

---
+++ Hook.js
@@ -0,0 +1,39 @@
+const http = require("http");
+
+//Parameters
+const host = 'your.ip';
+const port = 4444;
+
+const requestListener = function (req, res) {
+
+	//Client
+	res.writeHead(200);
+	res.end("H00K3D!");
+
+	//Server
+	let date_ob = new Date();
+	let hours = date_ob.getHours();
+	let minutes = date_ob.getMinutes();
+
+	process.stdout.write('\t' + '--- NEW REQUEST --- (' + `${hours}h${minutes}` + ')'+ '\n')
+	
+	process.stdout.write(req.method + ' from ' + req.socket.remoteAddress + '\n');
+	process.stdout.write(`http://${host}:${port}`+ req.url + '\n\n')
+
+	process.stdout.write('parameters : ' + '\n');
+	let params = req.url.split('?');
+	params = params[1]
+	params = params.split('&')
+	params.forEach(element => {
+		sub = element.split('=')
+		process.stdout.write('  ' + sub[0] + ' = ' + sub[1] + '\n');
+	  });
+	process.stdout.write('\n\n')
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+	process.stdout.write('\n' + `WebHook at http://${host}:${port}` + '\n\n');
+});
+
+
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # SimpleHook
+
+
+## Descripion
 A simple WebHook written in Node to perform test on web pages.
+
+## Install
+        ### 1) Clone the Project
+        ```
+        git clone https://github.com/TheoM83/SimpleHook
+        ```
+        
+        ### 2) Modify Parameters in Hook.js
+        ```
+        //Parameters
+        const host = 'your.ip';
+        const port = 4444;
+        ```
+        
+        ### 3) Run the Script
+        ```
+        node Hook.js
+        ```
+        
+        
